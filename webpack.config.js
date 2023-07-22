0b300a651147e0ffce39c34013a8a9d5d70bd673
Fuz7 <124787267+Fuz7@users.noreply.github.com>
2023-07-22 12:27:00 +0800

    Make dist more clean by adding image dir

--- webpack.config.js
+++ webpack.config.js
@@ -46,11 +46,10 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource'
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
+        generator: {
+          filename: 'image/[name][ext]' // Output image filename with image/ prefix
+        }
       },
       {
         test: /\.(csv|tsv)$/i,
